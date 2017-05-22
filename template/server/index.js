8d127429d500d49687cb6e448c734261e529c3ec
Sébastien Chopin <seb@chopin.io>
2017-05-22 11:17:21 +0200

    Works with 1.0

--- template/server/index.js
+++ template/server/index.js
@@ -12,23 +12,28 @@ app.set('port', port)
 // Import API Routes
 app.use('/api', api)
 
-// Import and Set Nuxt.js options
-let config = require('../nuxt.config.js')
-config.dev = !(process.env.NODE_ENV === 'production')
-
-// Init Nuxt.js
-const nuxt = new Nuxt(config)
-app.use(nuxt.render)
-
-// Build only in dev mode
-if (config.dev) {
-  nuxt.build()
-  .catch((error) => {
-    console.error(error) // eslint-disable-line no-console
-    process.exit(1)
-  })
+// Start nuxt.js
+async function start() {
+  // Import and Set Nuxt.js options
+  let config = require('../nuxt.config.js')
+  config.dev = !(process.env.NODE_ENV === 'production')
+  // Instanciate nuxt.js
+  const nuxt = await new Nuxt(config)
+  // Add nuxt.js middleware
+  app.use(nuxt.render)
+  // Build in development
+  if (config.dev) {
+    try {
+      await nuxt.build()
+    } catch (e) {
+      console.error(error) // eslint-disable-line no-console
+     process.exit(1)
+    }
+  }
+  // Listen the server
+  app.listen(port, host)
+  console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
 }
 
-// Listen the server
-app.listen(port, host)
-console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+start()
+
