d8c0a2fa973e1f649e8512ffd509f0d081ea4cd3
Sébastien Chopin <seb@chopin.io>
2017-06-04 15:27:17 +0200

    Update index.js

--- template/server/index.js
+++ template/server/index.js
@@ -18,18 +18,9 @@ async function start() {
   let config = require('../nuxt.config.js')
   config.dev = !(process.env.NODE_ENV === 'production')
   // Instanciate nuxt.js
-  const nuxt = await new Nuxt(config)
+  const nuxt = new Nuxt(config)
   // Add nuxt.js middleware
   app.use(nuxt.render)
-  // Build in development
-  if (config.dev) {
-    try {
-      await nuxt.build()
-    } catch (error) {
-      console.error(error) // eslint-disable-line no-console
-      process.exit(1)
-    }
-  }
   // Listen the server
   app.listen(port, host)
   console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
