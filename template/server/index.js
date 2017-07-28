555e392f9c7d4479c239cf1cba44711ba6591175
Sébastien Chopin <seb@chopin.io>
2017-07-28 10:54:19 +0200

    Use Nuxt 1.0 API

--- template/server/index.js
+++ template/server/index.js
@@ -1,5 +1,5 @@
-import Nuxt from 'nuxt'
 import express from 'express'
+import { Nuxt, Builder } from 'nuxt'
 
 import api from './api'
 
@@ -12,19 +12,22 @@ app.set('port', port)
 // Import API Routes
 app.use('/api', api)
 
-// Start nuxt.js
-async function start() {
-  // Import and Set Nuxt.js options
-  let config = require('../nuxt.config.js')
-  config.dev = !(process.env.NODE_ENV === 'production')
-  // Instanciate nuxt.js
-  const nuxt = new Nuxt(config)
-  // Add nuxt.js middleware
-  app.use(nuxt.render)
-  // Listen the server
-  app.listen(port, host)
-  console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+// Import and Set Nuxt.js options
+let config = require('./nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+// Init Nuxt.js
+const nuxt = new Nuxt(config)
+
+// Build only in dev mode
+if (config.dev) {
+  const builder = new Builder(nuxt)
+  builder.build()
 }
 
-start()
+// Give nuxt middleware to express
+app.use(nuxt.render)
 
+// Listen the server
+app.listen(port, host)
+console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
