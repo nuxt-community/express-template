091d5671b15974c4f5412e24cae88c0bfce0ba0b
Sébastien Chopin <seb@chopin.io>
2017-07-28 10:59:08 +0200

    Use process.server now

--- template/plugins/axios.js
+++ template/plugins/axios.js
@@ -2,7 +2,7 @@ import * as axios from 'axios'
 
 let options = {}
 // The server-side needs a full url to works
-if (process.SERVER_BUILD) {
+if (process.server) {
   options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`
 }
 
