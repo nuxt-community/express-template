b79d389b1f8c512a56b95e227112e1fe12bd5b97
Sébastien Chopin <seb@chopin.io>
2017-07-28 10:56:20 +0200

    Fix plugins path alias

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,7 +1,7 @@
 {{{{raw}}}}
 <template>
   <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
     <h1 class="title">
       USERS
     </h1>
@@ -17,7 +17,7 @@
 {{{{/raw}}}}
 
 <script>
-import axios from '~plugins/axios'
+import axios from '~/plugins/axios'
 
 export default {
   async asyncData () {
