1cb2131dbc9c9af4106dff9bf1301f32f3d6f38d
Sébastien Chopin <seb@chopin.io>
2017-07-28 10:58:42 +0200

    Fix plugins path alias

--- template/pages/_id.vue
+++ template/pages/_id.vue
@@ -1,7 +1,7 @@
 {{{{raw}}}}
 <template>
   <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
     <h1 class="title">
       User
     </h1>
@@ -16,7 +16,7 @@
 {{{{/raw}}}}
 
 <script>
-import axios from '~plugins/axios'
+import axios from '~/plugins/axios'
 
 export default {
   name: 'id',
