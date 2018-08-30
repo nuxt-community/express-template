7f2feae76d771f79229af4853b12023522e79524
GoT <PierreRambaud@users.noreply.github.com>
2018-08-30 16:56:37 +0200

    Typo fix
    No space before punctuation in english ;)

--- template/components/Footer.vue
+++ template/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer>
-    Visit our website for more documentation : <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
+    Visit our website for more documentation: <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
   </footer>
 </template>
