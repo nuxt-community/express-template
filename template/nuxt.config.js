c70e3a0c0b2c54d743438c687d955c91cda2f3d0
Baptiste <bapt.jan@gmail.com>
2019-03-26 16:32:01 +0100

    Replace deprecated nuxt config
    Use process.client instead of deprecated ctx.isClient

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -26,7 +26,7 @@ module.exports = {
     ** Run ESLINT on save
     */
     extend (config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
+      if (ctx.isDev && process.client) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
